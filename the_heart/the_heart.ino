006b38439ab185ebae7350909fbb7f2a8b4929d3
LIYOU ZHOU <leothemagnificent@gmail.com>
2017-07-30 18:22:42 +0100

    Add wifi and mqtt capabilities
    Now receives a mqtt string and displays it

--- the_heart/the_heart.ino
+++ the_heart/the_heart.ino
@@ -1,6 +1,3 @@
-// Adafruit_NeoMatrix example for single NeoPixel Shield.
-// Scrolls 'Howdy' across the matrix in a portrait (vertical) orientation.
-
 #include <Adafruit_GFX.h>
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
@@ -8,20 +5,62 @@
 
 #include "Font5x55pt7b.h"
 
+#include <ESP8266WiFi.h>
+#include <Adafruit_MQTT.h>
+#include <Adafruit_MQTT_Client.h>
+#include <IRremoteESP8266.h>
+
+/* for WLAN_SSID, WLAN_PASS, AIO_KEY */
+#include "credentials.h"
+
+/* for some handy colour names */
+#include "colours.h"
+
+/* Adafruit.io Setup */
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  8883                   // 8883 for MQTTS
+#define AIO_USERNAME    "liyouzhou"
+
+/* global state machine */
+typedef enum {
+    STATE_CONNECT,
+    STATE_LISTEN,
+    STATE_EMIT,
+    STATE_KEEP_ALIVE
+} the_heart_state_t;
+the_heart_state_t state = STATE_CONNECT;
+
+/* WiFiFlientSecure for SSL/TLS support */
+WiFiClientSecure client;
+
+/* Setup the MQTT client class by passing in the WiFi client and MQTT server and login details. */
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+/* io.adafruit.com SHA1 fingerprint */
+const char* fingerprint = "AD 4B 64 B3 67 40 B5 FC 0E 51 9B BD 25 E9 7F 88 B6 2A A3 5B";
+
+/* Setup a feed. Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname> */
+Adafruit_MQTT_Subscribe display_msg_feed = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/f/hdisp");
+
+/* workaround for spurious messages on first connect */
+bool first_connect = false;
+
+/* buffer to hold incoming mqtt messages */
+char msg_buf[100] = {0};
+
+/* neopixel pin */
 #define PIN D4
 
+/* globals to be used by text scroller */
 Ticker scroller;
-String s_global;
+char scroll_buf[100] = {0};
 int16_t X, Y;
 
+/* declear the display matrix global */
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(9, 9, PIN,
                                                NEO_MATRIX_BOTTOM + NEO_MATRIX_LEFT +
                                                NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE,
                                                NEO_GRB + NEO_KHZ800);
-
-const uint16_t colors[] = {
-    matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255)
-};
 
 uint16_t myRemapFn(uint16_t x_in, uint16_t y_in)
 {
@@ -71,10 +110,10 @@ void scroll()
     matrix.setCursor(X, Y);
 
     /* print text */
-    matrix.print(s_global);
+    matrix.print(scroll_buf);
 
     /* detect if scroll past end of text */
-    matrix.getTextBounds((char*)s_global.c_str(), X, Y, &x1, &y1, &w, &h);
+    matrix.getTextBounds((char*)scroll_buf, X, Y, &x1, &y1, &w, &h);
     if ( (x1 + w) <= 0 )
     {
         /* reset to start from outside of display boundary again */
@@ -98,15 +137,130 @@ void scrollText()
     scroller.attach_ms(100, scroll);
 }
 
+void verifyFingerprint()
+{
+    const char* host = AIO_SERVER;
+
+    Serial.print("Connecting to ");
+    Serial.println(host);
+
+    if (!client.connect(host, AIO_SERVERPORT))
+    {
+        Serial.println("Connection failed. Halting execution.");
+        while (1);
+    }
+
+    if (client.verify(fingerprint, host))
+    {
+        Serial.println("Connection secure.");
+    }
+    else
+    {
+        Serial.println("Connection insecure! Halting execution.");
+        while (1);
+    }
+}
+
+/* Function to connect and reconnect as necessary to the MQTT server.
+ * Should be called in the loop function and it will take care if connecting.
+ */
+void MQTT_connect()
+{
+    int8_t ret;
+
+    /* Stop if already connected. */
+    if (mqtt.connected())
+    {
+        return;
+    }
+
+    Serial.print("Connecting to MQTT... ");
+
+    uint8_t retries = 10;
+    while ((ret = mqtt.connect()) != 0)
+    {
+        /* connect will return 0 for connected */
+        Serial.println(mqtt.connectErrorString(ret));
+        Serial.println("Retrying MQTT connection in 5 seconds...");
+        mqtt.disconnect();
+        delay(5000);  // wait 5 seconds
+        retries--;
+        if (retries == 0)
+        {
+            /* die and wait for WDT to reset */
+            while (1);
+        }
+    }
+
+    first_connect = true;
+    Serial.println("MQTT Connected!");
+}
+
+void mqtt_msg_callback(char* buf, uint16_t len)
+{
+    /* print message */
+    Serial.print("Received: ");
+    for (uint16_t i = 0; i < len; i++)
+    {
+        Serial.print(buf[i]);
+    }
+    Serial.println();
+
+    if (len >= sizeof(msg_buf))
+    {
+        Serial.println("Message Too Long!!!");
+        return;
+    }
+
+    memcpy(msg_buf, buf, len);
+    msg_buf[len] = 0;
+
+    state = STATE_EMIT;
+}
+
+void emit()
+{
+    if (!first_connect)
+    {
+        uint16_t len = strnlen(msg_buf, 100);
+        if (len > 0 && len < 100)
+        {
+            memcpy(scroll_buf, msg_buf, len);
+            scroll_buf[len] = 0;
+            scrollText();
+        }
+    }
+
+    first_connect = false;
+}
+
 void setup()
 {
-    Serial.begin(9600);
+    Serial.begin(115200);
+
+    /* Connect to WiFi access point. */
+    Serial.print("Connecting to ");
+    Serial.println(WLAN_SSID);
+
+    WiFi.begin(WLAN_SSID, WLAN_PASS);
+
+    if (WiFi.waitForConnectResult() != WL_CONNECTED)
+    {
+        Serial.println("Unable to connect ot WIFI");
+    }
+
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    /* check the fingerprint of io.adafruit.com's SSL cert */
+    verifyFingerprint();
 
     /* setup display */
     matrix.begin();
     matrix.setTextWrap(false);
     matrix.setBrightness(100);
-    matrix.setTextColor(colors[0]);
+    matrix.setTextColor(RED);
     matrix.setFont(&Font5x55pt7b);
     matrix.setRemapFunction(myRemapFn);
 
@@ -114,16 +268,50 @@ void setup()
     matrix.fillScreen(0);
 
     /* draw a heart */
-    matrix.drawBitmap(1, 2, large_heart, 7, 7, colors[0]);
+    matrix.drawBitmap(1, 2, large_heart, 7, 7, RED);
     matrix.show();
+
+    /* setup feed callback and subscribe */
+    display_msg_feed.setCallback(mqtt_msg_callback);
+    mqtt.subscribe(&display_msg_feed);
 }
 
 void loop()
 {
-    if (Serial.available())
+    static unsigned long last_ping = millis();
+
+    switch (state)
     {
-        s_global = Serial.readString();
-        Serial.print(s_global);
-        scrollText();
+        case STATE_CONNECT:
+            Serial.println("STATE_CONNECT");
+            MQTT_connect();
+            state = STATE_LISTEN;
+            break;
+
+        case STATE_LISTEN:
+            Serial.println("STATE_LISTEN");
+            mqtt.processPackets(1000);
+            if (state == STATE_LISTEN)
+            {
+                state = STATE_KEEP_ALIVE;
+            }
+            break;
+
+        case STATE_EMIT:
+            Serial.println("STATE_EMIT");
+            emit();
+            state = STATE_CONNECT;
+            break;
+
+        case STATE_KEEP_ALIVE:
+            Serial.println("STATE_KEEP_ALIVE");
+            if (millis() - last_ping > 30 * 1000)
+            {
+                Serial.println("Pinging");
+                mqtt.ping();
+                last_ping = millis();
+            }
+            state = STATE_CONNECT;
+            break;
     }
 }
